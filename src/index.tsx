167d4cc5ef3e3056b52d9aad8c0e9feeda61c1fc
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2021-01-04 17:31:55 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -54,7 +54,7 @@ function Updaters() {
 }
 
 ReactDOM.render(
-  <StrictMode>
+  <>
     <FixedGlobalStyle />
     <Web3ReactProvider getLibrary={getLibrary}>
       <Web3ProviderNetwork getLibrary={getLibrary}>
@@ -69,6 +69,6 @@ ReactDOM.render(
         </Provider>
       </Web3ProviderNetwork>
     </Web3ReactProvider>
-  </StrictMode>,
+  </>,
   document.getElementById('root')
 )
