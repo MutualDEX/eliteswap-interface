a3a95dec7528d354b0bf39b899cbeca5a73be450
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2021-01-05 08:59:01 -0500

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,6 +12,7 @@ type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
 
+export const LINK = new Token(ChainId.MAINNET, '0x514910771AF9Ca656af840dff83E8264EcF986CA', 18, 'LINK', 'Chainlink')
 export const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin')
 export const USDC = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC', 'USD//C')
 export const USDT = new Token(ChainId.MAINNET, '0xdAC17F958D2ee523a2206206994597C13D831ec7', 6, 'USDT', 'Tether USD')
@@ -19,6 +20,57 @@ export const COMP = new Token(ChainId.MAINNET, '0xc00e94Cb662C3520282E6f57172140
 export const MKR = new Token(ChainId.MAINNET, '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2', 18, 'MKR', 'Maker')
 export const AMPL = new Token(ChainId.MAINNET, '0xD46bA6D942050d489DBd938a2C909A5d5039A161', 9, 'AMPL', 'Ampleforth')
 export const WBTC = new Token(ChainId.MAINNET, '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', 8, 'WBTC', 'Wrapped BTC')
+
+export const addressMap = {
+  uniswapFactory: '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95',
+  uniswapFactoryV2: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+  YFI: '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e',
+  YCRV: '0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8',
+  UNIAmpl: '0xc5be99a02c6857f9eac67bbce58df5572498f40c',
+  WETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  UNIRouter: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+  LINK: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
+  MKR: '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
+  SNX: '0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F',
+  COMP: '0xc00e94Cb662C3520282E6f5717214004A7f26888',
+  LEND: '0x80fB784B7eD66730e8b1DBd9820aFD29931aab03',
+  SUSHIYCRV: '0x2C7a51A357d5739C5C74Bf3C96816849d2c9F726',
+}
+
+export const contractAddresses = {
+  sushi: {
+    1: '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2',
+  },
+  masterChef: {
+    1: '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd',
+  },
+  weth: {
+    1: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  },
+  xSushi: {
+    1: '0x8798249c2e607446efb7ad49ec89dd1865ff4272'
+  }
+}
+
+/*
+SLP Address on mainnet for reference
+==========================================
+0  USDT 0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852
+1  USDC 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc
+2  DAI  0xa478c2975ab1ea89e8196811f51a7b7ade33eb11
+3  sUSD 0xf80758ab42c3b07da84053fd88804bcb6baa4b5c
+4  COMP 0xcffdded873554f362ac02f8fb1f02e5ada10516f
+5  LEND 0xab3f9bf1d81ddb224a2014e98b238638824bcf20
+6  SNX  0x43ae24960e5534731fc831386c07755a2dc33d47
+7  UMA  0x88d97d199b9ed37c29d846d00d443de980832a22
+8  LINK 0xa2107fa5b38d9bbd2c461d6edf11b11a50f6b974
+9  BAND 0xf421c3f2e695c2d4c0765379ccace8ade4a480d9
+10 AMPL 0xc5be99a02c6857f9eac67bbce58df5572498f40c
+11 YFI  0x2fdbadf3c4d5a8666bc06645b8358ab803996e28
+12 SUSHI 0xce84867c3c02b05dc570d0135103d3fb9cc19433
+*/
+
+
 
 // Block time here is slightly higher (~1s) than average in order to avoid ongoing proposals past the displayed time
 export const AVERAGE_BLOCK_TIME_IN_SECS = 14
